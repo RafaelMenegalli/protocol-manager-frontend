aaecad5ec902e0a7e5dab71a9447cd3914e1744d
Rafael Menegalli <rafaelmenegalli@gmail.com>
2024-07-28 00:35:38 -0300

    Finalização da listagem da tela principla

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/Header";
 import Link from "next/link";
 import dayjs from "dayjs";
 import { GetServerSideProps } from "next";
 
-import { Input, InputGroup, DatePicker, SelectPicker, Stack, Toggle, Button, toaster, Notification } from "rsuite";
+import Router from "next/router";
+
+import { Input, InputGroup, DatePicker, SelectPicker, Stack, Toggle, Button, toaster, Notification, Table, Col } from "rsuite";
+const { Column, HeaderCell, Cell } = Table;
 
 import { api } from "@/services/apiClient";
 
@@ -24,13 +27,75 @@ interface People {
   name: string;
 }
 
-interface Props {
-  listPeople: People[],
-  listDocuments: Document[]
+interface RawListProps {
+  id: string;
+  name: string;
+  initial_date: string;
+  final_date: string;
+  partial_answer: boolean;
+  right: boolean;
+  createdAt: string;
+  updatedAt: string;
+  people_id: string;
+  document_id: string;
 }
 
-export default function Home({ listDocuments, listPeople }: Props) {
+interface Props {
+  listPeople: People[],
+  listDocuments: Document[],
+  rawList: RawListProps[]
+}
+
+interface FormattedList {
+  id: string;
+  name: string;
+  initial_date: string;
+  final_date: string;
+  document: string;
+  people: string;
+  partial_answer: string;
+  right: string;
+}
+
+export default function Home({ listDocuments, listPeople, rawList }: Props) {
   const [loading, setLoading] = useState<boolean>(false);
+
+  const [rawListState, setRowListState] = useState<RawListProps[]>(rawList)
+  const [filteratedData, setFilteratedData] = useState<FormattedList[]>([]);
+
+  useEffect(() => {
+    if (rawListState) {
+      const formattedData = rawListState.map((item) => {
+        const formattedInitialDate = dayjs(item.initial_date).format("DD/MM/YYYY");
+        const formattedFinalDate = dayjs(item.final_date).format("DD/MM/YYYY");
+
+        const formattedPartialResponse = item.partial_answer ? "Sim" : "Não";
+        const formattedOk = item.right ? "OK" : "Não OK";
+
+        const documentName = listDocuments.find((doc) => doc.id === item.document_id)?.name || "";
+        const peopleName = listPeople.find((person) => person.id === item.people_id)?.name || "";
+
+        console.log({documentName})
+        console.log({peopleName})
+
+        return {
+          id: item.id,
+          name: item.name,
+          initial_date: formattedInitialDate,
+          final_date: formattedFinalDate,
+          document: documentName,
+          people: peopleName,
+          partial_answer: formattedPartialResponse,
+          right: formattedOk
+        };
+
+      });
+
+      setFilteratedData(formattedData);
+    }
+
+  }, [rawListState, listDocuments, listPeople])
+
   const [protocol, setProtocol] = useState<string>("");
   const [initialDate, setInitialDate] = useState<Date | null>(null);
   const [finalDate, setFinalDate] = useState<Date | null>(null);
@@ -44,20 +109,62 @@ export default function Home({ listDocuments, listPeople }: Props) {
   };
 
   async function handleRegisterProtocol() {
-    console.log({protocol})
-    console.log({initialDate})
-    console.log({finalDate})
-    console.log({selectedDocument})
-    console.log({selectedPeople})
-    console.log({partialResponse})
-    console.log({ok})
+    setLoading(true)
 
-    if(!protocol || !initialDate || !finalDate || !selectedDocument || !selectedPeople) {
+    if (!protocol || !initialDate || !finalDate || !selectedDocument || !selectedPeople) {
+      setLoading(false)
+
       toaster.push(
         <Notification type="warning" header="Aviso!">
           Preencha todos os dados para cadastrar!
+        </Notification>, { placement: 'topEnd', duration: 3500 }
+      )
+
+      return;
+    }
+
+    try {
+      const response = await api.post("/protocol", {
+        name: protocol,
+        initial_date: initialDate,
+        final_date: finalDate,
+        partial_answer: partialResponse,
+        right: ok,
+        people_id: selectedPeople,
+        document_id: selectedDocument
+      })
+
+      
+      toaster.push(
+        <Notification type="success" header="Sucesso!">
+          Protocolo cadastrado com sucesso!
+        </Notification>, { placement: 'topEnd', duration: 1500 }
+      )
+      
+      setProtocol("")
+      setInitialDate(null)
+      setFinalDate(null)
+      setSelectedDocument(null)
+      setSelectedPeople(null)
+      setPartialResponse(false)
+      setOk(false)
+      
+      setLoading(false)
+      
+      setTimeout(() => {
+        Router.reload()
+      }, 2000)
+      
+    } catch (error) {
+      setLoading(false)
+
+      toaster.push(
+        <Notification type="error" header="Erro!">
+          Este protocolo já está cadastrado!
         </Notification>, {placement: 'topEnd', duration: 3500}
       )
+
+      console.log("Erro ao cadastrar protocolo :::::>> ", error)
     }
   }
 
@@ -146,7 +253,52 @@ export default function Home({ listDocuments, listPeople }: Props) {
               Cadastrar
             </Button>
           </div>
+          <Table
+            height={450}
+            data={filteratedData}
+          >
+            <Column flexGrow={1}>
+              <HeaderCell>Protocolo</HeaderCell>
+              <Cell dataKey="name" />
+            </Column>
+
+            <Column flexGrow={1}>
+              <HeaderCell>Data Inícial</HeaderCell>
+              <Cell dataKey="initial_date" />
+            </Column>
+
+            <Column flexGrow={1}>
+              <HeaderCell>Data Final</HeaderCell>
+              <Cell dataKey="final_date" />
+            </Column>
+
+            <Column flexGrow={1} resizable>
+              <HeaderCell>Documento</HeaderCell>
+              <Cell dataKey="document" />
+            </Column>
+
+            <Column flexGrow={1} resizable>
+              <HeaderCell>Pessoa</HeaderCell>
+              <Cell dataKey="people" />
+            </Column>
+
+            <Column flexGrow={1}>
+              <HeaderCell>Resposta Parcal</HeaderCell>
+              <Cell dataKey="partial_answer" />
+            </Column>
+
+            <Column flexGrow={1}>
+              <HeaderCell>OK</HeaderCell>
+              <Cell dataKey="right" />
+            </Column>
+
+            <Column flexGrow={1}>
+              <HeaderCell>Ações</HeaderCell>
+              <Cell> Ação! </Cell>
+            </Column>
+          </Table>
         </div>
+
       </div>
     </>
   );
@@ -158,10 +310,13 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
     const people = await api.get("/peoples")
 
+    const rawList = await api.get("/protocols")
+
     return {
       props: {
         listDocuments: documents.data,
-        listPeople: people.data
+        listPeople: people.data,
+        rawList: rawList.data
       }
     }
   } catch (error) {
@@ -170,7 +325,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     return {
       props: {
         listDocuments: [],
-        listPeople: []
+        listPeople: [],
+        rawList: []
       }
     }
   }
